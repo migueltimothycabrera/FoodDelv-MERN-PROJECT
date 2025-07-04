4291973d126b0bf10e18840043e350530b2f472e
Miguel Cabrera <migueltimothycabrera@gmail.com>
2025-07-05 00:57:19 +0800

    first commit - commiting all frontend files

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState ({});
+    
+    const addtoCart = (itemId) => {
+        if(!cartItems[itemId]){
+            setCartItems(prev => ({...prev, [itemId]: 1}));
+        }
+        else {
+            setCartItems(prev => ({...prev, [itemId]: prev[itemId] + 1}))
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems(prev => ({...prev, [itemId]: prev[itemId] - 1}))
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
+
+    const constextValue = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addtoCart, 
+        removeFromCart
+    }
+    return (
+        <StoreContext.Provider value={constextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )  
+}
+export default StoreContextProvider;
